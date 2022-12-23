06dfa6a9ae9c4322a0174b5e54289f73ccf1e035
Tong Long <tongdamthanglong@gmail.com>
2022-12-23 12:56:44 +0700

    product schema

---
+++ server/models/product.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxlength: 160,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      maxlength: 2000,
+      required: true,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
