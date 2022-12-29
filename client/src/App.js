688d782cd749e7a773123e6774364443959dd8b1
Tong Long <tongdamthanglong@gmail.com>
2022-12-29 23:02:01 +0700

    Login Post request successful and file auth organize

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Menu from "./components/nav/Menu";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 
 function App() {
---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // res.data
+      const { data } = await axios.post(`${process.env.REACT_APP_API}/login`, {
+        email,
+        password,
+      });
+      console.log(data);
+
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        toast.success("Successful!");
+      }
+
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed!");
+    }
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                className="form-control mb-4 p-2"
+                placeholder="Enter email.."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                className="form-control mb-4 p-2"
+                placeholder="Enter password.."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="btn btn-info text-light" type="submit">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
