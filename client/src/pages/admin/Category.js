d03ade6244d28b6f3ba2b39106d9deb8cc7680b2
Tong Long <tongdamthanglong@gmail.com>
2023-01-05 18:53:52 +0700

    Admin Create and List Category

--- client/src/pages/admin/Category.js
+++ client/src/pages/admin/Category.js
@@ -1,10 +1,44 @@
-import React from "react";
-import { useAuth } from "../../context/auth";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 import AdminMenu from "../../components/nav/AdminMenu";
 
 const AdminCategory = () => {
-  const [auth, setAuth] = useAuth();
+  const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/category", { name });
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        // goi func loadCategories() de khi create xong thi list ra luon category, kh can refresh mount
+        loadCategories();
+        toast.success(`${data.name} created!`);
+      }
+      setName("");
+    } catch (error) {
+      console.log(error);
+      toast.error("Create Category Failed..");
+    }
+  };
+
   return (
     <>
       <div className="container-fluid">
@@ -14,7 +48,31 @@ const AdminCategory = () => {
           </div>
           <div className="col-md-9">
             <div className="p-3 mt-2 mb-2 h4 bg-light">Manage Categories</div>
-            <p>Create Category Form..</p>
+            <div className="p-3">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  className="form-control p-3"
+                  placeholder="Category Name.."
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <button type="submit" className="btn btn-info mt-3 mb-3">
+                  Add Category
+                </button>
+              </form>
+              <hr />
+              <div className="col">
+                {categories?.map((category) => (
+                  <button
+                    key={category._id}
+                    className="btn btn-outline-info mt-3 me-3"
+                  >
+                    {category.name}
+                  </button>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
