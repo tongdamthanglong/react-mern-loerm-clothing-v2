214b6e666cc959ada1760f17bfecd06063874688
Tong Long <tongdamthanglong@gmail.com>
2023-01-07 13:31:16 +0700

    File Upload in Create Product by Admin

--- client/src/pages/admin/Product.js
+++ client/src/pages/admin/Product.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import AdminMenu from "../../components/nav/AdminMenu";
 
+import { Select } from "antd";
+const { Option } = Select;
+
 const AdminProduct = () => {
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="container-fluid">
@@ -12,7 +38,42 @@ const AdminProduct = () => {
           </div>
           <div className="col-md-9">
             <div className="p-3 mt-2 mb-2 h4 bg-light">Create Products</div>
-            <p>Create Product Form..</p>
+            {photo && (
+              <div className="text-center">
+                <img
+                  src={URL.createObjectURL(photo)}
+                  alt="Product Image"
+                  className="img img-responsive"
+                  height="200px"
+                />
+              </div>
+            )}
+            <div className="pt-2">
+              <label className="btn btn-outline-info p-2 col-12 mb-3">
+                {photo ? photo.name : "Upload Photo"}
+                <input
+                  type="file"
+                  name="photo"
+                  accept="image/*"
+                  onChange={(e) => setPhoto(e.target.files[0])}
+                  hidden
+                />
+              </label>
+            </div>
+            <Select
+              showSearch
+              bordered={false}
+              size="large"
+              className="form-select mb-3"
+              placeholder="Choose Category"
+              onChange={(value) => setCategory(value)}
+            >
+              {categories?.map((category) => (
+                <Option key={category._id} value={category.name}>
+                  {category.name}
+                </Option>
+              ))}
+            </Select>
           </div>
         </div>
       </div>
