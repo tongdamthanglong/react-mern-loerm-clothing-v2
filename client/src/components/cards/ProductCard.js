e4c2473137803db1dd603ccb5c802ef0dcfb30ab
Tong Long <tongdamthanglong@gmail.com>
2023-01-10 19:39:28 +0700

    product ribbon

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Badge } from "antd";
 import moment from "moment";
 
 const ProductCard = ({ product }) => {
   return (
     <div className="card mb-3">
-      <img
-        src={`${process.env.REACT_APP_API}/product/photo/${product._id}`}
-        alt={product?.name}
-        style={{ height: "300px", objectFit: "cover" }}
-      />
+      <Badge.Ribbon text={`${product?.sold} sold`} color="cyan">
+        <Badge.Ribbon
+          text={product?.quantity >= 1 ? "In Stock" : "Out of stock"}
+          placement="start"
+          color="red"
+        >
+          <img
+            className="card-img-top"
+            src={`${process.env.REACT_APP_API}/product/photo/${product._id}`}
+            alt={product?.name}
+            style={{ height: "300px", objectFit: "cover" }}
+          />
+        </Badge.Ribbon>
+      </Badge.Ribbon>
+
       <div className="card-body">
         <h5>{product?.name}</h5>
         <p className="card-text">{product?.description?.substring(0, 60)}...</p>
