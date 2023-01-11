a7a3d154ca13a8dba1b4d6f13d57f07e59370fed
Tong Long <tongdamthanglong@gmail.com>
2023-01-11 13:59:52 +0700

    jumbotron styles

--- client/src/components/cards/Jumbotron.js
+++ client/src/components/cards/Jumbotron.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Jumbotron = ({ title, subtitle }) => {
   return (
-    <div className="container-fluid bg-info">
+    <div className="container-fluid jumbotron" style={{ height: "300px" }}>
       <div className="row">
-        <div className="col text-center p-5 bg-light">
-          <h1 className="mb-2">{title}</h1>
-          <p className="lead">{subtitle}</p>
+        <div className="col text-center p-5">
+          <h1 className="mb-2 mt-5 text-light">{title}</h1>
+          <p className="lead text-light">{subtitle}</p>
         </div>
       </div>
     </div>
