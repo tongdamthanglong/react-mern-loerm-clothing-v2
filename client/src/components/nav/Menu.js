a89669b5d9bbfd8b16110e1568bec0caf8800093
Tong Long <tongdamthanglong@gmail.com>
2023-01-13 14:30:54 +0700

    search box and menu nav style

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -16,64 +16,96 @@ const Menu = () => {
 
   return (
     <>
-      <ul className="nav d-flex justify-content-center mt-3 mb-4">
-        <li className="nav-item">
-          <NavLink className="nav-link" aria-current="page" to="/">
-            Home
+      <div className="d-flex justify-content-between align-items-center ps-2 pe-4">
+        <div
+          className="d-flex justify-content-between align-items-center gap-3 mt-3 mb-4"
+          style={{ width: "50%" }}
+        >
+          <NavLink
+            className="text-info fw-semibold text-center"
+            style={{ margin: "0px", width: "30%", textDecoration: "none" }}
+            to="/"
+          >
+            LOERM SHOP
           </NavLink>
-        </li>
+          <form className="d-flex" style={{ width: "60%" }}>
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Search Product.."
+              style={{ borderRadius: "0px", paddingRight: "80px" }}
+            />
+            <button
+              className="btn btn-info"
+              type="submit"
+              style={{ marginLeft: "-72px", borderRadius: "0px" }}
+            >
+              Search
+            </button>
+          </form>
+        </div>
 
-        <li className="nav-item">
-          <NavLink className="nav-link" aria-current="page" to="/shop">
-            Shop
-          </NavLink>
-        </li>
-
-        {!auth?.user ? (
-          <>
+        <div>
+          <ul className="nav d-flex justify-content-center mt-3 mb-4">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/login">
-                Login
+              <NavLink className="nav-link" aria-current="page" to="/">
+                Home
               </NavLink>
             </li>
+
             <li className="nav-item">
-              <NavLink className="nav-link" to="/register">
-                Register
+              <NavLink className="nav-link" aria-current="page" to="/shop">
+                Shop
               </NavLink>
             </li>
-          </>
-        ) : (
-          <div className="dropdown">
-            <li>
-              <a
-                className="nav-link pointer dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                {auth?.user?.name}
-              </a>
 
-              <ul className="dropdown-menu">
-                <li>
-                  <NavLink
-                    className="nav-link"
-                    to={`/dashboard/${
-                      auth?.user?.role === 1 ? "admin" : "user"
-                    }`}
-                  >
-                    Dashboard
+            {!auth?.user ? (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/login">
+                    Login
                   </NavLink>
                 </li>
-
-                <li className="nav-item pointer">
-                  <a onClick={handleLogout} className="nav-link">
-                    Logout
-                  </a>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/register">
+                    Register
+                  </NavLink>
                 </li>
-              </ul>
-            </li>
-          </div>
-        )}
-      </ul>
+              </>
+            ) : (
+              <div className="dropdown">
+                <li>
+                  <a
+                    className="nav-link pointer dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                  >
+                    {auth?.user?.name}
+                  </a>
+
+                  <ul className="dropdown-menu">
+                    <li>
+                      <NavLink
+                        className="nav-link"
+                        to={`/dashboard/${
+                          auth?.user?.role === 1 ? "admin" : "user"
+                        }`}
+                      >
+                        Dashboard
+                      </NavLink>
+                    </li>
+
+                    <li className="nav-item pointer">
+                      <a onClick={handleLogout} className="nav-link">
+                        Logout
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </div>
+            )}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
